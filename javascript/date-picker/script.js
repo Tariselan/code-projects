58b0ccb92f6d09e78b48e3f1df0458a5e29da3bc
silly girl :3 <woodmoreaaron@gmail.com>
2024-02-23 10:28:48 +1300

    com

---
+++ javascript/date-picker/script.js
@@ -0,0 +1,6 @@
+document.getElementById('date1').addEventListener('input', function() {
+    const date = document.getElementById('date1');
+    console.log(date.value)
+})
+
+let date1 = new Date()
